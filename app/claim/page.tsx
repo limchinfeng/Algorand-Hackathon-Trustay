738b461b865f47072f7c0a6ba95fde85d60433b6
Perming1126 <perming@student.usm.my>
2023-10-18 11:28:26 +0800

    page setup

--- app/claim/page.tsx
+++ app/claim/page.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
 
 export default function page() {
+    const reportData = [
+        {
+            item: "The guest has broken furnitures in the house",
+
+        },
+        {
+            item: "The guest has unsettled outstanding utilities fee",
+
+        },
+        {
+            item: "The guest has made the place extremely dirty and hard to clean.",
+
+        },
+        {
+            item: "The guest has committed crime at the place. ",
+
+        },
+        {
+            item: "The guest has unsettled outstanding rental.",
+
+        },
+    ];
+
     return (
-        <div>page</div>
+        <div className="mx-50px">
+            <h1 className="text-2xl font-bold ">Report and Claim</h1>
+            <h1 className="text-2xl font-bold ">What has the guest breached?</h1>
+            <div>
+                {reportData.map((data, index) => {
+                    return (
+                        <div key={index} className="">
+                            {data.item}
+                        </div>
+                    )
+                })}
+            </div>
+
+        </div>
+
 
 
     )
